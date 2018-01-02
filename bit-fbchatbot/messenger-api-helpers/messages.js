0d08f9190f25f05831c806825101a2539313b829
yeonju <byj714012@gmail.com>
2018-01-03 01:14:32 +0900

    .

--- bit-fbchatbot/messenger-api-helpers/messages.js
+++ bit-fbchatbot/messenger-api-helpers/messages.js
@@ -1,26 +1,4 @@
-const getStarted = {
-  greeting:[
-    {
-      "locale":"default",
-      "text":"Hello!"
-    }, {
-      "locale":"en_US",
-      "text":"Timeless apparel for the masses."
-    }
-  ],
-    setting_type: 'call_to_actions',
-    thread_state: 'new_thread',
-    call_to_actions: [
-      {
-        //payload: JSON.stringify({
-          //type: 'GET_STARTED',
-        //}),
-        payload: 'GET_STARTED'
-      },
-      
-    ],
-  
-  }
+
   const signInButton = {
     type: 'account_link',
     //url: `${SERVER_URL}/users/login`,
