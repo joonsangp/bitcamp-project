76fd3aacb617818d376ecaf4660577ba7a0ac338
yeonju <byj714012@gmail.com>
2018-01-02 20:35:40 +0900

    .

---
+++ bit-fbchatbot/messenger-api-helpers/messages.js
@@ -0,0 +1,18 @@
+const getStarted = {
+    setting_type: 'call_to_actions',
+    thread_state: 'new_thread',
+    call_to_actions: [
+      {
+        //payload: JSON.stringify({
+          //type: 'GET_STARTED',
+        //}),
+        payload: 'GET_STARTED'
+      },
+      
+    ]
+  
+  }
+  module.exports =  {
+    getStarted,
+  };
+  
