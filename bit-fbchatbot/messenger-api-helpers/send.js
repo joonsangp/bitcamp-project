a2f9c51262fecb8ce5c94b3ade475b0871cb79c0
yeonju <byj714012@gmail.com>
2018-01-02 20:06:03 +0900

    .

--- bit-fbchatbot/messenger-api-helpers/send.js
+++ bit-fbchatbot/messenger-api-helpers/send.js
@@ -1,155 +1,138 @@
 const api = require('./api')
 
+// 기존 함수
 const sendTextMessage = (recipientId, messageText) => {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: messageText
-      }
-    };
-    api.callMessagesAPI(messageData);
-    console.log('Test =======>'+ messageText);
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+  api.callMessagesAPI(messageData);
 };
 
 const sendImageMessage = (recipientId) => {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        "attachment":{
-          "type":"image",
-          "payload":{
-          "url":"http://ppss.kr/wp-content/uploads/2016/04/%ED%8A%B8%EB%9F%BC%ED%94%8401-549x411.jpg",
-          "is_reusable":true
-          }
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "메인 메뉴 사진",
+            subtitle: "메뉴 사진",
+            image_url: "http://www.bloter.net/wp-content/uploads/2016/08/%EC%8A%A4%EB%A7%88%ED%8A%B8%ED%8F%B0-%EC%82%AC%EC%A7%84-765x519.jpg",
+            
+            buttons: [{
+              type: "web_url",
+              url: "http://www.jawsfood.co.kr/?page_id=202",
+              title: "사이트 방문"
+            },{
+              type: "postback",
+              payload: "/menu",
+              title: "메인으로"
+            }]
+
+          }]
         }
       }
-    };
-
-    api.callMessagesAPI(messageData);
+    }
+  }; 
+  api.callMessagesAPI(messageData);
 };
 
-const sendButton1Message = (recipientId) => {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        "attachment":{
-          "type":"template",
-          "payload":{
-            "template_type":"button",
-            "text":"검색 사이트를 선택하세요!",
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://www.google.com",
-                "title":"구글"
-              },
-              {
-                "type":"web_url",
-                "url":"https://www.bing.com",
-                "title":"빙"
-              },
-              {
-                "type":"web_url",
-                "url":"https://www.naver.com",
-                "title":"네이버"
-              }
-            ]
-          }
-        }
-      }
-    };
-
-    api.callMessagesAPI(messageData);
-};
-
-const sendButton2Message = (recipientId) => {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        "attachment":{
-          "type":"template",
-          "payload":{
-            "template_type":"button",
-            "text":"전화걸기",
-            "buttons":[
-              {
-                "type":"phone_number",
-                "title":"내 전화번호",
-                "payload":"+821036749313"
-              }
-            ]
-          }
-        }
-      }
-    };
-
-    api.callMessagesAPI(messageData);
-};
 
 const sendGenericMessage = (recipientId) => {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [{
-              title: "rift",
-              subtitle: "Next-generation virtual reality",
-              item_url: "https://www.oculus.com/en-us/rift/",
-              image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-              buttons: [{
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/rift/",
-                title: "Open Web URL"
-              }, {
-                type: "postback",
-                title: "Call Postback",
-                payload: "Payload for first bubble",
-              }],
-            }, {
-              title: "touch",
-              subtitle: "Your Hands, Now in VR",
-              item_url: "https://www.oculus.com/en-us/touch/",
-              image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-              buttons: [{
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/touch/",
-                title: "Open Web URL"
-              }, {
-                type: "postback",
-                title: "Call Postback",
-                payload: "Payload for second bubble",
-              }]
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "이벤트 메뉴 사진 1",
+            subtitle: "12월달 신메뉴",
+            image_url: "http://www.bloter.net/wp-content/uploads/2016/08/%EC%8A%A4%EB%A7%88%ED%8A%B8%ED%8F%B0-%EC%82%AC%EC%A7%84-765x519.jpg",
+            
+            buttons: [{
+              type: "web_url",
+              url: "http://www.jawsfood.co.kr/?page_id=202",
+              title: "사이트 방문"
+            },{
+              type: "postback",
+              payload: "/menu",
+              title: "메인으로"
+            }
+          ]
+
+          }, {
+            title: "이벤트 메뉴 사진 2",
+            subtitle: "할인 이벤트",
+            image_url: "http://www.bloter.net/wp-content/uploads/2016/08/%EC%8A%A4%EB%A7%88%ED%8A%B8%ED%8F%B0-%EC%82%AC%EC%A7%84-765x519.jpg",
+
+            buttons: [{
+              type: "web_url",
+              url: "http://www.jawsfood.co.kr/?page_id=206",
+              title: "사이트 방문"
+            },{
+              type: "postback",
+              payload: "/menu",
+              title: "메인으로"
             }]
-          }
+            
+          },{
+            title: "이벤트 메뉴 사진 3",
+            subtitle: "적립 이벤트",
+            image_url: "http://www.bloter.net/wp-content/uploads/2016/08/%EC%8A%A4%EB%A7%88%ED%8A%B8%ED%8F%B0-%EC%82%AC%EC%A7%84-765x519.jpg",
+
+            buttons: [{
+              type: "web_url",
+              url: "http://www.jawsfood.co.kr/?page_id=202",
+              title: "사이트 방문"
+            },{
+              type: "postback",
+              payload: "/menu",
+              title: "메인으로"
+            }]
+            
+          },{
+            title: "이벤트 메뉴 사진 4",
+            subtitle: "공짜 이벤트",
+            image_url: "http://www.bloter.net/wp-content/uploads/2016/08/%EC%8A%A4%EB%A7%88%ED%8A%B8%ED%8F%B0-%EC%82%AC%EC%A7%84-765x519.jpg",
+
+            buttons: [{
+              type: "web_url",
+              url: "http://www.jawsfood.co.kr/?page_id=206",
+              title: "사이트 방문"
+            },{
+              type: "postback",
+              payload: "/menu",
+              title: "메인으로"
+            }]
+            
+          }]
         }
       }
-    };
-
-    api.callMessagesAPI(messageData);
+    }
+  };  
+  api.callMessagesAPI(messageData);
 };
-
 
 
 
 module.exports = {
 
   sendTextMessage,
-  sendImageMessage,
-  sendButton1Message,
-  sendButton2Message,
-  sendGenericMessage
+  sendGenericMessage,
+  sendImageMessage
+
 
 };
