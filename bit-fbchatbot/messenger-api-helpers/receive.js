7729e56229087173b26a0d5f27981b06ecd25091
yeonju <byj714012@gmail.com>
2018-01-03 18:32:55 +0900

    .

--- bit-fbchatbot/messenger-api-helpers/receive.js
+++ bit-fbchatbot/messenger-api-helpers/receive.js
@@ -20,12 +20,12 @@ const handleReceiveMessage = (event) => {
     var handler = messageHandler.getHandler(messageText);
 
     if(handler) { // 메시지를 처리할 함수가 있다면
+      sendAPI.sendReadReceipt(senderID);
       handler(senderID); // 그함수 호츨    
     } else if (menu) {
       /* handler = messageHandler.getHandler(menu); // 사용자 현재 메뉴의 메시지를 처리할 함수를 꺼낸다
       handler(senderID, messageText);
       */
-      sendAPI.sendReadReceipt(senderID);
     
     } else {
       sendAPI.sendTextMessage(senderID, '유효한 명령이 아닙니다.')
