836a5b1d459081706a6229c5ec8ca2738b5f4922
yeonju <byj714012@gmail.com>
2018-01-03 20:04:02 +0900

    .

--- bit-fbchatbot/messenger-api-helpers/receive.js
+++ bit-fbchatbot/messenger-api-helpers/receive.js
@@ -22,6 +22,8 @@ const handleReceiveMessage = (event) => {
     
     if(handler) { // 메시지를 처리할 함수가 있다면
      // sendAPI.sendReadReceipt(senderID);
+     sendAPI.typingOff(senderID);
+     
       handler(senderID); // 그함수 호츨    
     } else if (menu) {
       /* handler = messageHandler.getHandler(menu); // 사용자 현재 메뉴의 메시지를 처리할 함수를 꺼낸다
