66531489f66ca406d49d1a989380efe5acfcdddf
yeonju <byj714012@gmail.com>
2018-01-04 16:25:03 +0900

    .

--- bit-fbchatbot/routes/webhook.js
+++ bit-fbchatbot/routes/webhook.js
@@ -81,10 +81,10 @@ router.post('/', (req, res) => {
 
         } else if (event.postback) {
 
-           if (event.postback.payload === 'GET_STARTED') {  // 사용자가 있거나 처음 시작한다면 if문 수행
-             console.log('event.postback===>11111111111111 ', event.postback)
-             receiveAPI.handleReceivePostback(event);
-           }
+          //  if (event.postback.payload === 'GET_STARTED') {  // 사용자가 있거나 처음 시작한다면 if문 수행
+          //    console.log('event.postback===>11111111111111 ', event.postback)
+          //    receiveAPI.handleReceivePostback(event);
+          //  }
           console.log('event.postback===> ', event.postback)
           receiveAPI.handleReceivePostback(event);
 
