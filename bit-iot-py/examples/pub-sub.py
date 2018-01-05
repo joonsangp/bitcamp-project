2770745a9f858d6aa9536c3eebca73ae636869f4
yeonju <byj714012@gmail.com>
2018-01-05 11:15:20 +0900

    .

--- bit-iot-py/examples/pub-sub.py
+++ bit-iot-py/examples/pub-sub.py
@@ -6,7 +6,7 @@ import argparse
 import json
 import humidifier_api as humidifier
 import ventilator_api as ventilator
-
+'''
 def customCallback1(client, userdata, message):
     print("메시지를 수신하였습니다. \n")
     print("사서함 이름: ")
@@ -39,7 +39,7 @@ def customCallback2(client, userdata, message):
         ventilator.onVentilator(False)
     print("--------------")
 
-'''
+
 
 host = "a3urzfjm9f14zj.iot.ap-northeast-2.amazonaws.com"
 rootCAPath = "../root-CA.crt"
@@ -75,6 +75,6 @@ print("connect! \n")
 
 # AWS IoT의 Thing의 'topic_1' 사서함을 구독하겠다고 선언
 # 메시지를 받으면 customCallback 함수가 호출될 것이다.
-myAWSIoTMQTTClient.subscribe(topic, 1, customCallback1)
+# myAWSIoTMQTTClient.subscribe(topic, 1, customCallback1)
 
-# myAWSIoTMQTTClient.subscribe(topic, 1, customCallback2)
+ myAWSIoTMQTTClient.subscribe(topic, 1, customCallback2)
