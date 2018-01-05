f51d2b6c1f977f86ef48b3715fe8bbaf6a9d308d
yeonju <byj714012@gmail.com>
2018-01-05 11:13:56 +0900

    .

--- bit-iot-py/examples/pub-sub.py
+++ bit-iot-py/examples/pub-sub.py
@@ -22,7 +22,7 @@ def customCallback1(client, userdata, message):
     else :
         humidifier.onHumidifier(False)
     print("--------------")
-
+'''
 def customCallback2(client, userdata, message):
     print("메시지를 수신하였습니다. \n")
     print("사서함 이름: ")
@@ -39,7 +39,7 @@ def customCallback2(client, userdata, message):
         ventilator.onVentilator(False)
     print("--------------")
 
-
+'''
 
 host = "a3urzfjm9f14zj.iot.ap-northeast-2.amazonaws.com"
 rootCAPath = "../root-CA.crt"
@@ -77,4 +77,4 @@ print("connect! \n")
 # 메시지를 받으면 customCallback 함수가 호출될 것이다.
 myAWSIoTMQTTClient.subscribe(topic, 1, customCallback1)
 
-myAWSIoTMQTTClient.subscribe(topic, 1, customCallback2)
+# myAWSIoTMQTTClient.subscribe(topic, 1, customCallback2)
