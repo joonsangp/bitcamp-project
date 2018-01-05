237c9f3e7f3c2a30e820305091b804fd8a71b59a
yeonju <byj714012@gmail.com>
2018-01-05 11:17:24 +0900

    .

--- bit-iot-py/examples/pub-sub.py
+++ bit-iot-py/examples/pub-sub.py
@@ -76,5 +76,4 @@ print("connect! \n")
 # AWS IoT의 Thing의 'topic_1' 사서함을 구독하겠다고 선언
 # 메시지를 받으면 customCallback 함수가 호출될 것이다.
 # myAWSIoTMQTTClient.subscribe(topic, 1, customCallback1)
-
- myAWSIoTMQTTClient.subscribe(topic, 1, customCallback2)
+myAWSIoTMQTTClient.subscribe(topic, 1, customCallback2)
